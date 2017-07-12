36fa4e78eac7b432735e25e5d0e235983f72216a
Deni Shakh <admin@MacBook-dl.local>
2017-07-12 16:03:09 -0400

    I'm extra

--- scrape.py
+++ scrape.py
@@ -1,19 +1,39 @@
-from lxml import html
 import requests
+from bs4 import BeautifulSoup
+import csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-page = open("Disease.html")
-tree = html.fromstring(page.content)
+update_id = None
+url = 'http://people.dbmi.columbia.edu/~friedma/Projects/DiseaseSymptomKB/index.html'
 
-#This will create a list of buyers:
-#buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-#This will create a list of prices
-#prices = tree.xpath('//span[@class="item-price"]/text()')
-#print(buyers)
-#print(prices)
+webpage = requests.get(url)
+soup = BeautifulSoup(webpage.text, 'html.parser')
 
-#page = requests.get('http://people.dbmi.columbia.edu/~friedma/Projects/DiseaseSymptomKB/index.html')
-#tree = html.fromstring(page.content)
-#table//MsoTableWeb3
-tr = tree.xpath('//table[@class="MsoTableWeb3"]//tbody//tr/text()')
+name_box = soup.find('table', attrs={'class': 'MsoTableWeb3'})
+name = name_box.text.strip()
+name = name[54:]
+data =  list(name)
+counter = 0
+intermidiate = ''
 
-print(tr)
+for x in range(0, len(data)):
+	if(data[x] == '\n'):
+		counter = counter + 1
+		if(counter == 4):
+			intermidiate = intermidiate + '\n'
+			counter = 0
+		elif(counter == 3 and data[x+1]!='\n'):
+			intermidiate = intermidiate + ','
+			counter = 0
+		elif(counter==1 and data[x+1]!='\n'):
+			counter = 0
+			
+	else:
+		intermidiate = intermidiate + data[x]
+
+with open('csvfile.csv','wb') as file:
+    for line in intermidiate:
+        file.write(line)
+        
